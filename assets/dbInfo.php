c6a20ee36d1b570ce9e6b41129260b13264ee960
Yuzhou Guo <35606080+YuzhouGuo@users.noreply.github.com>
2019-07-09 12:22:11 -0400

    Update dbInfo.php

--- assets/dbInfo.php
+++ assets/dbInfo.php
@@ -3,7 +3,7 @@
 //create an object of connection to be called query with
 $connection = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
 
-$query = "SELECT firstName, lastName, birthday, email, comments FROM Authors ORDER BY first_name";
+$query = "SELECT firstName, lastName, birthday, email, comments FROM theBase ORDER BY first_name";
 $resultObj = $connection->query($query);
 
 ?>
